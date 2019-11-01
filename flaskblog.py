19136bb456c30697fbb123d1a8b58d951aeed458
Marcin Weglowski <mcweglowski@gmail.com>
2019-11-01 13:17:56 +0100

    Added home and about routes

--- flaskblog.py
+++ flaskblog.py
@@ -3,6 +3,14 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+@app.route('/home')
+def home():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return f'<H1>Hello, {escape(name)}!<H1>'
+
+@app.route('/about')
+def about():
+    return f'<H1>About page<H1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
