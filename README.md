c9c7ac02a62a316ef036a93c74e4b561f156f7c6
Marcin Weglowski <mcweglowski@gmail.com>
2019-11-01 16:40:50 +0100

    Basic version of Home and About. Basic layout.html

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Python\Flask training project.
 Configuration:
 $env:FLASK_APP='flaskblog.py'
 $env:FLASK_DEBUG=1
+
 Get-ChildItem env:
 Get-ChildItem env:FLASK_APP
--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,30 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Marcin Weglowski',
+        'title': 'Blog post 1',
+        'content' : 'First post content.',
+        'date_posted':'31.10.2019',
+    },
+    {
+        'author':'Grzegorz Braun',
+        'title': 'Blog post 2',
+        'content' : 'Second post content.',
+        'date_posted':'01.11.2019',
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    name = request.args.get("name", "World")
-    return f'<H1>Hello, {escape(name)}!<H1>'
+    return render_template('home.html', posts = posts)
 
 @app.route('/about')
 def about():
-    return f'<H1>About page<H1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
