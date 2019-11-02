36194a4eb0a45469c734ad21b4e5ba3ab956990c
Marcin Weglowski <mcweglowski@gmail.com>
2019-11-02 13:45:55 +0100

    Use url_for in layout.html instead of direct links

--- flaskblog.py
+++ flaskblog.py
@@ -37,9 +37,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@admin.pl" and form.password.data == "admin":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check Username and Password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
