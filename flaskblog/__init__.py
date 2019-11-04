cbd193fdf5909e90c03df5ac9dba717a5fd2685e
Marcin Weglowski <mcweglowski@gmail.com>
2019-11-04 06:53:37 +0100

    Model scratch. flaskblog_tools initial work

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1a4d1aebb222cd6d0dd481f4f8def584'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskblog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../flaskblog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
