f443e4e6825e5ea7890e0d6989a37899234c17fc
Marcin Weglowski <mcweglowski@gmail.com>
2019-11-11 11:50:45 +0100

    Add is_active flag in the user

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -14,6 +14,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=True, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
