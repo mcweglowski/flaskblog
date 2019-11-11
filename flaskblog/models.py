1251ba36883ede73325f7f952926062fac853eef
Marcin Weglowski <mcweglowski@gmail.com>
2019-11-11 21:36:36 +0100

    Configure Flask-Migrations

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -14,7 +14,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=True, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    is_active = db.Column(db.Boolean, nullable=False, default=True)
+    is_active = db.Column(db.Boolean, nullable=True, default=True)
+    is_banned = db.Column(db.Boolean, nullable=True, default=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
